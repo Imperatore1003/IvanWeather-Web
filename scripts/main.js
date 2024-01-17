c4de55451e7c671ee7a7c3da7e8b9870ab5482ce
Ivan Beltrame <87547424+Imperatore1003@users.noreply.github.com>
2024-01-17 21:57:32 +0100

    Improvments

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,12 @@
-// const radios = document.querySelectorAll('.form-check-input')
-// for (const radio of radios) {
-//     radio.onclick = (e) => {
-//         let units = document.querySelector("label[for='" + radio.id + "']").innerHTML.trim();
-//         units = units === "Celsius" ? 1 : 0;
-//         let url = window.location.href;
-
-//         let dict_values = {units, url} //Pass the javascript variables to a dictionary.
-//         let s = JSON.stringify(dict_values); // Stringify converts a JavaScript object or value to a JSON string
-//         $.ajax({
-//             url:"/setUnits",
-//             type:"POST",
-//             contentType: "application/json",
-//             data: JSON.stringify(s),
-//             complete: function (data) {
-//                 window.location.href = url;
-//             }
-//         });
-//     }
-// }
+// Mode 0 = City Name
+// Mode 1 = Latitude and Longitude
+// Unit 0 = Imperial
+// Unit 1 = Metric
 
 const APIKEY = "84b3fab687879f825802c0ad57147510";
+const cookiesExpiresDays = 1;
 
+// Load the home page weather
 function home() {
     if (getCookie("latitude") != "" && getCookie("longitude") != "") {
         document.getElementById("container").style.display = "block";
@@ -36,7 +22,7 @@ function home() {
         getLocation();
     }
 }
-
+// Load the search page weather
 function search() {
     cityName = GetParameter("city");
 
@@ -48,57 +34,7 @@ function search() {
     displayWeather(city);
 }
 
-function setCookie(name, value) {
-    const d = new Date();
-    const exdays = 1;
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    setCookie("latitude", latitude);
-    setCookie("longitude", longitude);
-    location.reload();
-}
-
-function error() {
-    mystatus('Unable to retrieve your location');
-}
-
-function mystatus(status) {
-    document.getElementById('status').innerHTML = "<p>" + status + "</p>";
-}
-
-function getLocation() {
-   if (!navigator.geolocation) {
-        mystatus('Geolocation is not supported by your browser');
-    } else {
-        mystatus("Getting your location...");
-        navigator.geolocation.getCurrentPosition(success, error);
-    } 
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-// Mode 0 = City Name
-// Mode 1 = Latitude and Longitude
+// Get the weather from the API
 function getWeather(cityName, latitude = 0, longitude = 0, mode = 0, unit = 0) {
     let url = "https://api.openweathermap.org/data/2.5/weather?lang=en&appid=" + APIKEY
     
@@ -114,10 +50,11 @@ function getWeather(cityName, latitude = 0, longitude = 0, mode = 0, unit = 0) {
         url += "&units=metric";
     }
 
-    // Ritorna una stringa statica per non consumare chiamate a OpenWeatherMap - TEST API RESPONSE
+    // TEST API RESPONSE
     return '{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":32.77,"feels_like":25.79,"temp_min":29.75,"temp_max":36.18,"pressure":993,"humidity":80},"visibility":10000,"wind":{"speed":8.01,"deg":40,"gust":13},"clouds":{"all":100},"dt":1705521210,"sys":{"type":2,"id":268730,"country":"GB","sunrise":1705478291,"sunset":1705508528},"timezone":0,"id":2643743,"name":"London","cod":200}';
 }
 
+// Display the weather in the container
 function displayWeather(city, metric = 0) {
     city = JSON.parse(city);
 
@@ -134,11 +71,8 @@ function displayWeather(city, metric = 0) {
     document.getElementById("spHumidity").innerHTML = city["main"]["humidity"];
 
     if (metric == 0) {
-        // document.getElementById("spVisibility").innerHTML = Math.round(city["wind"]["speed"] * 2.237, 2);
         document.getElementById("spVisibility").innerHTML = (city["wind"]["speed"] * 2.237).toFixed(2);
-        // document.getElementById("spPressure").innerHTML = Math.round(city["main"]["pressure"] / 33.864 , 2);
         document.getElementById("spPressure").innerHTML = (city["main"]["pressure"] / 33.864).toFixed(2);
-        // document.getElementById("spWind").innerHTML = Math.round(city["visibility"] / 1.609, 2);
         document.getElementById("spWind").innerHTML = (city["visibility"] / 1.609).toFixed(2);
 
     } else {
@@ -147,7 +81,6 @@ function displayWeather(city, metric = 0) {
         document.getElementById("spWind").innerHTML = city["wind"]["speed"];
     }
 
-    // document.getElementById("spDewPoint").innerHTML = Math.round(city["main"]["temp"] - ((100 - city["main"]["humidity"]) / 5), 2);
     document.getElementById("spDewPoint").innerHTML = (city["main"]["temp"] - ((100 - city["main"]["humidity"]) / 5)).toFixed(2);
 
     let sunrise = new Date((parseInt(city["sys"]["sunrise"]) + parseInt(city["timezone"])) * 1000);
@@ -172,6 +105,67 @@ function displayWeather(city, metric = 0) {
     document.getElementById("spImg").innerHTML = '<img src="https://openweathermap.org/img/wn/' + city["weather"][0]["icon"] + '.png" alt="Icon" style="width: 50px; height: 50px;">';
 }
 
+// Set the unit in a cookie
+function setUnit(unit) {
+    if (unit == 0) {
+        setCookie("unit", "imperial", 364);
+    } else {
+        setCookie("unit", "metric", 364);
+    }
+}
+
+// Set a cookie
+function setCookie(name, value, additionalDays = 0) {
+    const d = new Date();
+    d.setTime(d.getTime() + ((cookiesExpiresDays + additionalDays) * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+// Get a cookie
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+// Function to display a message
+function mystatus(status) {
+    document.getElementById('status').innerHTML = "<p>" + status + "</p>";
+}
+// Function to get the location
+function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    setCookie("latitude", latitude);
+    setCookie("longitude", longitude);
+    location.reload();
+}
+// Display an error message if the location cannot be retrieved
+function error() {
+    mystatus('Unable to retrieve your location');
+}
+
+// Get the location of the user from the browser
+function getLocation() {
+    if (!navigator.geolocation) {
+         mystatus('Geolocation is not supported by your browser');
+     } else {
+         mystatus("Getting your location...");
+         navigator.geolocation.getCurrentPosition(success, error);
+     } 
+ }
+
+// Get a parameter from the URL
 function GetParameter(parameterName) {
     var result = null,
         tmp = [];
